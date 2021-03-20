77e4ef0442dea72b0f2b118ad15581e78f28311c
albimc <56416069+albimc@users.noreply.github.com>
2021-03-20 19:34:36 +0000

    Added Pong REINFORCE folder

---
+++ pong/pong-REINFORCE.py
@@ -0,0 +1,7 @@
+# custom utilies for displaying animation, collecting rollouts and more
+import pong_utils
+
+# check which device is being used.
+# I recommend disabling gpu until you've made sure that the code runs
+device = pong_utils.device
+print("using device: ",device)
